cbe6093c9f148fede3e11584460c8806db42fb8d
syahdeee <100667458+syahdeee@users.noreply.github.com>
2023-07-20 19:13:54 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,28 +1,27 @@
 import streamlit as st
-# from streamlit_option_menu import option_menu
+from streamlit_option_menu import option_menu
 
-st.text_input("Nilai CO kendaraan (0 - 10):", 0)
 #navigasi
-# with st.sidebar:
-#     selected = option_menu("MENU", ["Home", 'Prediksi Rating'])
-#     selected
+with st.sidebar:
+    selected = option_menu("MENU", ["Home", 'Prediksi Rating'])
+    selected
 
-# if (selected == 'Home'):
-#     st.title("Koefisien Pajak Web Application")
-#     st.write("By : PSTB - BRIN team")
+if (selected == 'Home'):
+    st.title("Koefisien Pajak Web Application")
+    st.write("By : PSTB - BRIN team")
 
-# if (selected == 'Prediksi Rating'):
-#     # horizontal Menu
-#     selected2 = option_menu(None, ["Diesel", "Gasoline"],  
-#     orientation="horizontal")
+if (selected == 'Prediksi Rating'):
+    # horizontal Menu
+    selected2 = option_menu(None, ["Diesel", "Gasoline"],  
+    orientation="horizontal")
     
-#     if (selected2 == "Diesel"):
-#         opasitas = st.text_input("Opasitas kendaraan (angka 0 - 100):", 0)
-#         tahun = st.text_input("Tahun Pembuatan kendaraan (s/d 2023):", 0)
+    if (selected2 == "Diesel"):
+        opasitas = st.text_input("Opasitas kendaraan (angka 0 - 100):", 0)
+        tahun = st.text_input("Tahun Pembuatan kendaraan (s/d 2023):", 0)
         
-#         hitungD = st.button("HITUNG RATING")
-#     if (selected2 == "Gasoline"):
-#         tahun = st.text_input("Tahun Pembuatan kendaraan (s/d 2023):", 0)
-#         co = st.text_input("Nilai CO kendaraan (0 - 10):", 0)
-#         hc = st.text_input("Nilai HC kendaraan (0 - 10000):", 0)
-#         hitungG = st.button("HITUNG RATING")
+        hitungD = st.button("HITUNG RATING")
+    if (selected2 == "Gasoline"):
+        tahun = st.text_input("Tahun Pembuatan kendaraan (s/d 2023):", 0)
+        co = st.text_input("Nilai CO kendaraan (0 - 10):", 0)
+        hc = st.text_input("Nilai HC kendaraan (0 - 10000):", 0)
+        hitungG = st.button("HITUNG RATING")
